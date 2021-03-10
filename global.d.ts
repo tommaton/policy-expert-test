a514e567217bb4472917b2eb7f430892abac3d79
Tom Maton <gintomm@gmail.com>
2021-03-10 20:46:38 +0000

    chore(data): added product data and global types for reuse

---
+++ global.d.ts
@@ -0,0 +1,8 @@
+declare namespace PolicyExpert {
+    interface IProductData {
+        id: string;
+        name: string;
+        price: number;
+        unit: number;
+    }
+}
---
+++ src/data/products.ts
@@ -0,0 +1,20 @@
+export const productData: PolicyExpert.IProductData[] = [
+    {
+        id: '1',
+        name: 'Face Mask',
+        price: 2.50,
+        unit: 1
+    },
+    {
+        id: '2',
+        name: 'Toilet Paper',
+        price: 0.65,
+        unit: 1
+    },
+    {
+        id: '3',
+        name: 'Hand Sanitizer',
+        price: 19.99,
+        unit: 0.175
+    },
+]
