8cb3ac131a33803962ba18edc3eaba01c107e7cc
Tom Maton <gintomm@gmail.com>
2021-03-12 09:22:46 +0000

    feat(product): added product container and test

---
+++ src/components/product/src/product.container.test.tsx
@@ -0,0 +1,16 @@
+import { mapDispatchToProps } from './product.container';
+
+jest.mock('@pe/redux');
+
+describe('Product container', () => {
+  describe('mapDispatchToProps', () => {
+    const dispatch = jest.fn();
+    const props = mapDispatchToProps(dispatch);
+
+    it('should return an object containing actions', () => {
+      expect(props.addToBasket).toBeDefined();
+      props.addToBasket('id');
+      expect(dispatch).toHaveBeenCalledTimes(1);
+    });
+  });
+});
