a3dc679af9d3d4b50e4a567ead97a928be5272ad
Tom Maton <gintomm@gmail.com>
2021-03-12 14:01:58 +0000

    text(redux): new selectors test to get shopping list

---
+++ src/redux/basket/src/selectors.test.ts
@@ -0,0 +1,15 @@
+import { getShoppingList } from './selectors';
+
+describe('redux-basket', () => {
+  describe('selectors', () => {
+    describe('getBasket', () => {
+      const state = {
+        shoppingList: [],
+      };
+
+      it('should return a user object', () => {
+        expect(getShoppingList(state)).toBe(state.shoppingList);
+      });
+    });
+  });
+});
